438273c0ccc5d87148c67fbbad6d9436291e798d
Natalia Dygas <natalia.dygas@wp.pl>
2020-01-23 00:32:24 +0100

    Fix problem with appearing navbar + add done action after first edit

--- app/src/main/java/com/example/mobileprogramming/ui/AboutFragment.java
+++ app/src/main/java/com/example/mobileprogramming/ui/AboutFragment.java
@@ -1,11 +1,26 @@
 package com.example.mobileprogramming.ui;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobileprogramming.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,4 +32,37 @@ public class AboutFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.calc_fragment,container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        BottomNavigationView navBar = getActivity().findViewById(R.id.bottom_navigation);
+
+        EditText gram = getActivity().findViewById(R.id.weightValue);
+        EditText dishName = getActivity().findViewById(R.id.dishValue);
+        TextView title = getActivity().findViewById(R.id.dishName);
+
+        gram.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        gram.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (keyboardShown(gram.getRootView())) {
+                    navBar.setVisibility(View.GONE);
+                } else {
+                    navBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    private boolean keyboardShown(View rootView) {
+        final int softKeyboardHeight = 100;
+        Rect r = new Rect();
+        rootView.getWindowVisibleDisplayFrame(r);
+        DisplayMetrics dm = rootView.getResources().getDisplayMetrics();
+        int heightDiff = rootView.getBottom() - r.bottom;
+        return heightDiff > softKeyboardHeight * dm.density;
+    }
+
 }
