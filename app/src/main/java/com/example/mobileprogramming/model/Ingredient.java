34d0d0739786c50d7e1948975df87661a7b72258
Natalia Dygas <natalia.dygas@wp.pl>
2020-01-23 00:35:45 +0100

    Bind listview with adding products on calc fragment

---
+++ app/src/main/java/com/example/mobileprogramming/model/Ingredient.java
@@ -0,0 +1,38 @@
+package com.example.mobileprogramming.model;
+
+public class Ingredient {
+
+    private String imgUrl;
+    private String name;
+    private double weight;
+
+    public Ingredient(String imgUrl, String name, double weight) {
+        this.imgUrl = imgUrl;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
--- app/src/main/java/com/example/mobileprogramming/ui/AboutFragment.java
+++ app/src/main/java/com/example/mobileprogramming/ui/AboutFragment.java
@@ -5,6 +5,7 @@ import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.telecom.InCallService;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -14,35 +15,68 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mobileprogramming.R;
+import com.example.mobileprogramming.infrastructure.IngredientAdapter;
+import com.example.mobileprogramming.model.Ingredient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import androidx.fragment.app.Fragment;
 
 public class AboutFragment extends Fragment {
-    @Nullable
+
+    private List<Ingredient> ingredients = new ArrayList<>();
+    private IngredientAdapter adapter;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calc_fragment,container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.calc_fragment, container, false);
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         BottomNavigationView navBar = getActivity().findViewById(R.id.bottom_navigation);
 
+        ListView listView = getActivity().findViewById(R.id.ingrListView);
         EditText gram = getActivity().findViewById(R.id.weightValue);
         EditText dishName = getActivity().findViewById(R.id.dishValue);
         TextView title = getActivity().findViewById(R.id.dishName);
 
+        hideNavbarWhenKeyboardAppear(navBar, gram);
+
+        ingredients.add(new Ingredient("test","test", 2.0));
+        adapter = new IngredientAdapter(getContext(), R.layout.ingredient, ingredients);
+        listView.setAdapter(adapter);
+
+        Button button = getActivity().findViewById(R.id.addButton);
+
+        button.setOnClickListener(v -> {
+            double weight = Double.parseDouble(gram.getText().toString());
+            Ingredient ingredient = new Ingredient(
+                "dasd","dad" , weight
+            );
+            ingredients.add(ingredient);
+            adapter.notifyDataSetChanged();
+            gram.setText("");
+        });
+
+    }
+
+    private void hideNavbarWhenKeyboardAppear(BottomNavigationView navBar, EditText gram) {
         gram.setImeOptions(EditorInfo.IME_ACTION_DONE);
         gram.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
