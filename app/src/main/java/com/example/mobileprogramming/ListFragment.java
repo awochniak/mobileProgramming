e457e43ca7490c95fc4eb1edbc7f37315817b4c3
Joanna <j.glowacka15@gmail.com>
2020-01-23 00:12:54 +0100

    Firebase refactor

--- app/src/main/java/com/example/mobileprogramming/ListFragment.java
+++ app/src/main/java/com/example/mobileprogramming/ListFragment.java
@@ -4,47 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-import com.example.mobileprogramming.config.Config;
-import com.example.mobileprogramming.model.Product;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.mobileprogramming.service.DBHelper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ListFragment extends Fragment {
-    @Nullable
+
+    private final DBHelper dbHelper = new DBHelper();
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference(Config.PRODUCTS_COLLECTION_NAME);
-
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                List<Product> products = new ArrayList<>();
-                for (DataSnapshot ds: dataSnapshot.getChildren()){
-                    Product product = ds.getValue(Product.class);
-                    products.add(product);
-                }
-                System.out.println(products);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                //Log.e("Problem with DB connection");
-            }
-        });
-
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.list_fragment,container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView listView = view.findViewById(R.id.listView);
+
+        dbHelper.getProducts(products -> {
+            ProductsAdapter adapter = new ProductsAdapter(getContext(), R.layout.list_element, products);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener((adapterView, view1, i, l) -> System.out.println(i));
+        });
     }
 }
---
+++ app/src/main/java/com/example/mobileprogramming/service/DBHelper.java
@@ -0,0 +1,44 @@
+package com.example.mobileprogramming.service;
+
+import com.example.mobileprogramming.config.Config;
+import com.example.mobileprogramming.model.Product;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+
+public class DBHelper {
+    public void getProducts(FirebaseDBCallback callback){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference(Config.PRODUCTS_COLLECTION_NAME);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Product> products = new ArrayList<>();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    Product product = ds.getValue(Product.class);
+                    products.add(product);
+                }
+                callback.onCallback(products);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //Log.e("Problem with DB connection");
+            }
+        });
+    }
+
+    public interface FirebaseDBCallback {
+        void onCallback(List<Product> products);
+    }
+}
+
+
