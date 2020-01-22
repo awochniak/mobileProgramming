a0e1876fae5afa010818a7437e74979f61e756ee
Joanna <j.glowacka15@gmail.com>
2020-01-23 00:10:46 +0100

    Add firebase

--- app/src/main/java/com/example/mobileprogramming/ListFragment.java
+++ app/src/main/java/com/example/mobileprogramming/ListFragment.java
@@ -5,6 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mobileprogramming.config.Config;
+import com.example.mobileprogramming.model.Product;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,6 +24,27 @@ public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference(Config.PRODUCTS_COLLECTION_NAME);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Product> products = new ArrayList<>();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    Product product = ds.getValue(Product.class);
+                    products.add(product);
+                }
+                System.out.println(products);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //Log.e("Problem with DB connection");
+            }
+        });
+
+
         return inflater.inflate(R.layout.list_fragment,container, false);
     }
 }
---
+++ app/src/main/java/com/example/mobileprogramming/config/Config.java
@@ -0,0 +1,6 @@
+package com.example.mobileprogramming.config;
+
+public class Config {
+    public final static String PRODUCTS_COLLECTION_NAME = "products";
+    public final static String DB_ERROR = "Firebase Realtime DB: ";
+}
